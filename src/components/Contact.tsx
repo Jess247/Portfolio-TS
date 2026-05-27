695663260bc485241da7135b0ea0d210f6c06027
Jessica Christin Singletary <jsingletary@wbsedu.de>
2026-05-27 14:56:36 +0200

    Add social link icons

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,5 +1,9 @@
 import ButtonInverted from './ButtonInverted'
 import styles from '../styles/Contact.module.css'
+import { TiSocialGithub, TiSocialLinkedin   } from "react-icons/ti";
+import { IoIosMail, IoLogoWhatsapp, IoLogoGithub, IoLogoLinkedin   } from "react-icons/io";
+
+
 
 
 export default function Contact() {
@@ -8,10 +12,16 @@ export default function Contact() {
             <h2>Contact me</h2>
             <p>Sounds interesting? Follow me on Social Media or write a me a message.</p>
             <ul className={styles.links}>
-                <li><a href="">GH</a></li>
-                <li><a href="">LI</a></li>
-                <li><a href="">WA</a></li>
-                <li><a href="">Email</a></li>
+                <li>
+                    <a href=""><IoLogoGithub  size={30} /></a>
+                </li>
+                <li>
+                    <a href=""><IoLogoLinkedin  size={30} /></a>
+                    </li>
+                <li>
+                    <a href=""><IoLogoWhatsapp size={30} /></a>
+                </li>
+                <li><a href=""><IoIosMail size={30} /></a></li>
             </ul>
             <form action="" className={styles.form}>
                 <label htmlFor="name">Name: </label>
