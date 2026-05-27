119233328d1bf70b92a08ee0ded335497fd2839a
Jessica Christin Singletary <jsingletary@wbsedu.de>
2026-05-27 13:15:28 +0200

    Fix layout issues for mobile screens

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,17 +1,18 @@
-import Button from "./Button";
+import Button from './Button'
+import styles from '../styles/Contact.module.css'
 
 export default function Contact() {
     return(
-        <section>
+        <section className={styles.contactContainer}>
             <h2>Contact me</h2>
             <p>Sounds interesting? Follow me on Social Media or write a me a message.</p>
-            <ul>
+            <ul className={styles.links}>
                 <li><a href="">GH</a></li>
                 <li><a href="">LI</a></li>
                 <li><a href="">WA</a></li>
                 <li><a href="">Email</a></li>
             </ul>
-            <form action="">
+            <form action="" className={styles.form}>
                 <label htmlFor="name">Name: </label>
                 <input type="text" name="name" />
                 <label htmlFor="msg">Message: </label>
