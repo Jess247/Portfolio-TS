ddfce58afc8a5b85bb85f9503b95d1c44551c993
Jessica Christin Singletary <jsingletary@wbsedu.de>
2026-05-28 08:22:02 +0200

    Fix layout issues on large screens

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,18 +1,28 @@
 import styles from '../styles/Projects.module.css'
 import Button from './Button'
 
+const projects =[ 
+    { img:'https://cdn.dribbble.com/userupload/47873122/file/7fe9abe6e6b2731e3b5a796114b0fc39.png?crop=0x0-2400x1800&format=webp&resize=1200x900&vertical=center', name:'Project Name', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, quasi'},
+    { img:'https://cdn.dribbble.com/userupload/47873122/file/7fe9abe6e6b2731e3b5a796114b0fc39.png?crop=0x0-2400x1800&format=webp&resize=1200x900&vertical=center', name:'Project Name', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, quasi'},
+    { img:'https://cdn.dribbble.com/userupload/47873122/file/7fe9abe6e6b2731e3b5a796114b0fc39.png?crop=0x0-2400x1800&format=webp&resize=1200x900&vertical=center', name:'Project Name', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, quasi'},
+    { img:'https://cdn.dribbble.com/userupload/47873122/file/7fe9abe6e6b2731e3b5a796114b0fc39.png?crop=0x0-2400x1800&format=webp&resize=1200x900&vertical=center', name:'Project Name', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, quasi'}
+]
+
 export default function Projects() {
     return(
         <section className={styles.projectContainer}>
             <h2>Projects</h2>
-            <div className={styles.card}>
-                <img className={styles.img} src="https://cdn.dribbble.com/userupload/47863977/file/still-f964b533b3ac0fee5cccbbc879cdbae7.png?format=webp&resize=400x300&vertical=center" width="200px" alt="" />
-                <div className='text'>
-                    <h3>Project Name</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, quasi.</p>
-                    <Button text="View on Github"/>
-                </div>
-            </div>
+            {projects.map(project => (
+                            <div className={styles.card}>
+                                <img className={styles.img} src={project.img} width="200px" alt="" />
+                                <div className='text'>
+                                    <h3>{project.name}</h3>
+                                    <p>{project.description}</p>
+                                    <Button text="View on Github"/>
+                                </div>
+                            </div>
+            ))}
         </section>
     )
 }
+
