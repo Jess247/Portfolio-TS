16c7a8687710f14846f520637db41a208aaee65c
Jessica Christin Singletary <jsingletary@wbsedu.de>
2026-05-28 08:29:06 +0200

    Fix layout issues in contact section

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -9,27 +9,29 @@ import { IoIosMail, IoLogoWhatsapp, IoLogoGithub, IoLogoLinkedin   } from "react
 export default function Contact() {
     return(
         <section className={styles.contactContainer}>
-            <h2>Contact me</h2>
-            <p>Sounds interesting? Follow me on Social Media or write a me a message.</p>
-            <ul className={styles.links}>
-                <li>
-                    <a href=""><IoLogoGithub  size={30} /></a>
-                </li>
-                <li>
-                    <a href=""><IoLogoLinkedin  size={30} /></a>
+            <div className={styles.textContent}>
+                <h2>Contact me</h2>
+                <p>Sounds interesting? Follow me on Social Media or write a me a message.</p>
+                <ul className={styles.links}>
+                    <li>
+                        <a href=""><IoLogoGithub  size={30} /></a>
                     </li>
-                <li>
-                    <a href=""><IoLogoWhatsapp size={30} /></a>
-                </li>
-                <li><a href=""><IoIosMail size={30} /></a></li>
-            </ul>
-            <form action="" className={styles.form}>
-                <label htmlFor="name">Name: </label>
-                <input type="text" name="name" className={styles.input} />
-                <label htmlFor="msg">Message: </label>
-                <textarea name="msg" id="" className={styles.textarea}></textarea>
-                <ButtonInverted text="send"/>
-            </form>
+                    <li>
+                        <a href=""><IoLogoLinkedin  size={30} /></a>
+                        </li>
+                    <li>
+                        <a href=""><IoLogoWhatsapp size={30} /></a>
+                    </li>
+                    <li><a href=""><IoIosMail size={30} /></a></li>
+                </ul>
+                <form action="" className={styles.form}>
+                    <label htmlFor="name">Name: </label>
+                    <input type="text" name="name" className={styles.input} />
+                    <label htmlFor="msg">Message: </label>
+                    <textarea name="msg" id="" className={styles.textarea}></textarea>
+                    <ButtonInverted text="send"/>
+                </form>
+            </div>
         </section>
     )
 }
